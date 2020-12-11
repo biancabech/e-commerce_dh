0d02ef999a6f68f9019f5e1a40d66b062908723d
Dan2020-bit <daniele-maria2011@bol.com.br>
2020-12-10 23:17:19 -0300

    criacao pagina de cadastro

---
+++ klivid/controllers/cadastro-de-cliente.js
@@ -0,0 +1,8 @@
+
+const cadastroCliente = {
+    index: (req,res)=>{
+        res.render('cadastro-de-cliente')
+    }
+}
+
+module.exports = cadastroCliente;
--- klivid/routes/index.js
+++ klivid/routes/index.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var router = express.Router();
-
+const cadastroController = require('../controllers/cadastro-de-cliente')
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+router.get ('/cadastro', cadastroController.index)
 
 module.exports = router;
